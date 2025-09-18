efb07cf896f8e73f809a5beb4d2493d0b33ef2b3
DatXinh <140891709+DatXinh@users.noreply.github.com>
2025-09-18 23:27:33 +0700

    Update các kiểu bắn cho xe tank

--- Assets/Resource/NguyenDat/Script/BulletHitContact.cs
+++ Assets/Resource/NguyenDat/Script/BulletHitContact.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class BulletHitContact : MonoBehaviour
 {
-    private RoundBullet parentBullet;
-
-    void Start()
-    {
-        parentBullet = GetComponentInParent<RoundBullet>();
-    }
-
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Tank"))
         {
-            // Xử lý trúng tank
-            Debug.Log("Tank bị trúng đạn!");
-            Destroy(parentBullet.gameObject);
+            // Hủy object cha (bullet) khi va chạm
+            if (transform.parent != null)
+            {
+                Debug.Log("Hit: Tank");
+                Destroy(transform.parent.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 }
